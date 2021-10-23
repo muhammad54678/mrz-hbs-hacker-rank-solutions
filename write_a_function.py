f1a4136dc469e3f3ae683469c32e75b87f1c9b1d
muhammad54678 <93044720+muhammad54678@users.noreply.github.com>
2021-10-23 08:15:39 -0700

    Update and rename z.py to write_a_function.py

---
+++ write_a_function.py
@@ -0,0 +1,14 @@
+def is_leap(year):
+    leap_year = False
+    
+    # Write your logic here
+    if year % 400 == 0:
+        leap_year = True
+    elif year % 100 == 0:
+        leap_year = False
+    elif year % 4 == 0:
+        leap_year = True
+    return leap_year
+
+year = int(input())
+print(is_leap(year))
